489766696b23d590d844b9c67a8dbf1b9a2b15cb
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-09-30 17:50:50 -0300

    fix: changes function name

--- atividade_operadores_logicos.sql
+++ atividade_operadores_logicos.sql
@@ -1,7 +1,8 @@
 
 -- Atividade Aula 9 
---
+
 -- FUNCAO QUE VERIFICA SE UM NUM É multiplo de 3
+-- Exercicio 1.1
 DROP FUNCTION numero_multiplo_de_3;
 
 CREATE OR REPLACE FUNCTION numero_multiplo_de_3(numero INT)
@@ -16,5 +17,24 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Chama a função p/ teste
+-- Chama e testa a função
 SELECT numero_multiplo_de_3(9);
+
+-- Exercicio 1.2
+DROP FUNCTION numero_multiplo_de_5ou3;
+
+CREATE OR REPLACE FUNCTION numero_multiplo_de_5ou3(numero INT)
+RETURNS text -- Retorna texto com msg.
+AS $$
+BEGIN
+	IF numero % 5 = 0 or numero % 5 = 0 THEN
+	 RETURN 'O Número é multiplo de 3 ou 5';
+	ELSE
+	 RETURN 'O Número NÃO é multiplo de 3 ou 5';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Chama e testa a função
+SELECT numero_multiplo_de_5ou3(25);
+
