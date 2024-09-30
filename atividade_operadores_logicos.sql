f159040ebfb8a5232d19fa027a04e3fc64fe86bb
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-09-30 20:19:41 -0300

    feat: implements exerc 1.3 and 1.4

--- atividade_operadores_logicos.sql
+++ atividade_operadores_logicos.sql
@@ -20,6 +20,7 @@ $$ LANGUAGE plpgsql;
 -- Chama e testa a função
 SELECT numero_multiplo_de_3(9);
 
+
 -- Exercicio 1.2
 DROP FUNCTION numero_multiplo_de_5ou3;
 
@@ -37,4 +38,76 @@ $$ LANGUAGE plpgsql;
 
 -- Chama e testa a função
 SELECT numero_multiplo_de_5ou3(25);
+
+-- Exercicio 1.3
+CREATE OR REPLACE FUNCTION calculadora()
+RETURNS text
+AS $$
+DECLARE
+    numero1 INT;
+    numero2 INT;
+    operacao INT;
+    resultado INT;
+    operador TEXT;
+BEGIN
+
+    RAISE NOTICE 'Escolha uma operação:';
+    RAISE NOTICE '1 - Soma';
+    RAISE NOTICE '2 - Subtração';
+    RAISE NOTICE '3 - Multiplicação';
+    RAISE NOTICE '4 - Divisão';
+    
+    operacao := 1; 
+    numero1 := 2; 
+    numero2 := 3; 
+
+    IF operacao = 1 THEN
+        resultado := numero1 + numero2;
+        operador := '+';
+    ELSIF operacao = 2 THEN
+        resultado := numero1 - numero2;
+        operador := '-';
+    ELSIF operacao = 3 THEN
+        resultado := numero1 * numero2;
+        operador := '*';
+    ELSIF operacao = 4 THEN
+        IF numero2 = 0 THEN
+            RAISE NOTICE 'Erro: Divisão por zero!';
+            RETURN;
+        ELSE
+            resultado := numero1 / numero2;
+            operador := '/';
+        END IF;
+    ELSE
+        RAISE NOTICE 'Operação inválida!';
+        RETURN;
+    END IF;
+
+    RAISE NOTICE '% % % = %', numero1, operador, numero2, resultado;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Exercicio 1.4 
+CREATE OR REPLACE FUNCTION calculaValorDeVenda(custo NUMERIC)
+RETURNS NUMERIC AS $$
+DECLARE
+    valor_venda NUMERIC;
+BEGIN
+    IF custo < 20 THEN
+        valor_venda := custo * 1.45; 
+    ELSE
+        valor_venda := custo * 1.30; 
+    END IF;
+
+    RETURN valor_venda;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Chama e testa a funcao()
+SELECT calculaValorDeVenda(30)
+
+
+
+
 
